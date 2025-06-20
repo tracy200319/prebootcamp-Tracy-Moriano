0a06c3eddf541b5c4537a0637a6b130a62fdfe14
tracy200319 <tracynicolehmorianotuanama@gmail.com>
2025-06-20 17:43:40 -0500

    Update JS

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.getElementById("botonSaludo").addEventListener("click", function() {
   const nombre = document.getElementById("nombre").value;
-  const mensaje = `Hola, ${nombre}`;
+  const mensaje = `Hola ${nombre}, un gusto:p`;
   document.getElementById("mensaje").textContent = mensaje;
 }); 
