3c60781151715371ec0dacdd3483cc3aa97f9c03
tracy200319 <tracynicolehmorianotuanama@gmail.com>
2025-06-22 13:27:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.getElementById("botonSaludo").addEventListener("click", function() {
   const nombre = document.getElementById("nombre").value;
-  const mensaje = `Hola ${nombre}, un gusto, te saluda Tracy Moriano:p`;
+  const mensaje = `Hola ${nombre}, un gusto, te saluda Tracy Moriano`;
   document.getElementById("mensaje").textContent = mensaje;
 });  
